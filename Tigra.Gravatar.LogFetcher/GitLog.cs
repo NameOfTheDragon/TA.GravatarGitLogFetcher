f01952da743fee2b07789946490c9ff651c48d3d
Tim Long <Tim@tigranetworks.co.uk>
2019-10-17 01:06:38 +0100

    Tidied up , improved command line processing and error handling

--- Tigra.Gravatar.LogFetcher/GitLog.cs
+++ Tigra.Gravatar.LogFetcher/GitLog.cs
@@ -1,54 +1,92 @@
+// This file is part of the Tigra.Gravatar.LogFetcher project
+//
+// Copyright © 2016-2019 Tigra Astronomy, all rights reserved.
+//
+// File: GitLog.cs  Last modified: 2019-10-16@21:32 by Tim Long
+
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
-using System.Text;
-using TiGra;
+using NLog.Fluent;
 
 namespace Tigra.Gravatar.LogFetcher
-    {
+{
     public class GitLog
-        {
-        const string GitLogCommitterRegex = @"^(?<email>[^|]+)\|(?<name>.*)$";
-        FakeFileSystemWrapper fileSystem;
+    {
+        private const string GitLogCommitterRegex = @"^(?<email>[^|]+)\|(?<name>.*)$";
+        private readonly FakeFileSystemWrapper fileSystem;
 
         /// <summary>
-        ///   Initializes a new instance of the <see cref="GitLog" /> class.
+        ///     Initializes a new instance of the <see cref="GitLog" /> class.
         /// </summary>
         /// <param name="pathToWorkingCopy">The path to a Git working copy.</param>
-        /// <param name="fakeFileSystemWrapper">A helper class that provides file system services (optional).</param>
+        /// <param name="fakeFileSystemWrapper">
+        ///     A helper class that provides file system
+        ///     services (optional).
+        /// </param>
         /// <exception cref="ArgumentException">Thrown if the path is invalid.</exception>
-        /// <exception cref="InvalidOperationException">Thrown if there is no Git repository at the specified path.</exception>
+        /// <exception cref="InvalidOperationException">
+        ///     Thrown if there is no Git repository at
+        ///     the specified path.
+        /// </exception>
         public GitLog(string pathToWorkingCopy, FakeFileSystemWrapper fakeFileSystemWrapper = null)
-            {
-            this.fileSystem = fakeFileSystemWrapper ?? new FakeFileSystemWrapper();
-            string fullPath = fileSystem.GetFullPath(pathToWorkingCopy); // ArgumentException if path invalid.
+        {
+            fileSystem = fakeFileSystemWrapper ?? new FakeFileSystemWrapper();
+            var fullPath = fileSystem.GetFullPath(pathToWorkingCopy); // ArgumentException if path invalid.
             if (!fileSystem.DirectoryExists(fullPath))
                 throw new ArgumentException("The specified working copy directory does not exist.");
             GitWorkingCopyPath = pathToWorkingCopy;
-            string git = fileSystem.PathCombine(fullPath, ".git");
+            var git = fileSystem.PathCombine(fullPath, ".git");
             if (!fileSystem.DirectoryExists(git))
-                {
                 throw new InvalidOperationException(
                     "There does not appear to be a Git repository at the specified location.");
-                }
-            }
+        }
 
         public string GitWorkingCopyPath { get; set; }
 
+        public IEnumerable<Committer> GetListOfUniqueCommitters(ProcessStartInfo gitPsi = null)
+        {
+            var logStream = GetLogStream(gitPsi);
+            var uniqueCommitters = new SortedSet<Committer>();
+            var committersAdded = 0;
+            while (!logStream.EndOfStream)
+                try
+                {
+                    var logLine = logStream.ReadLine();
+                    Log.Debug().Message("Read from log stream => {logLine}", logLine).Write();
+                    var parts = logLine.Split('|');
+                    if (parts.Length != 2) // Must hav
+                        continue;
+                    if (string.IsNullOrEmpty(parts[0])) // Must have a non-blank email address
+                        continue;
+                    var committer = new Committer(parts[1], parts[0]);
+                    var added = uniqueCommitters.Add(committer);
+                    Log.Debug().Message("Committer={committer}, Added={added}", committer, added).Write();
+                    if (added)
+                        ++committersAdded;
+                }
+                catch (Exception ex)
+                {
+                    Log.Warn().Exception(ex).Write();
+                }
+            Log.Info().Message("Added a total of {count} committers", committersAdded).Write();
+            return uniqueCommitters;
+        }
+
         /// <summary>
-        /// Launches a command line process with the command
-        /// <c>git --git-dir="{path-to-working-copy}\.git" log --pretty=format:"%ae|%an"</c>
-        /// and captures stdout into a StreamReader object.
+        ///     Launches a command line process with the command
+        ///     <c>git --git-dir="{path-to-working-copy}\.git" log --pretty=format:"%ae|%an"</c>
+        ///     and captures stdout into a StreamReader object.
         /// </summary>
         /// <param name="psi">
-        /// The process start info containing the executable to be run.
-        /// Dependency Injection: a script can be supplied to mock out Git.
-        /// If null is passed, then "git.exe" is used by default.
+        ///     The process start info containing the executable to be run.
+        ///     Dependency Injection: a script can be supplied to mock out Git.
+        ///     If null is passed, then "git.exe" is used by default.
         /// </param>
         /// <returns>A StreamReader hooked up to the stdout of the git command process.</returns>
         public StreamReader GetLogStream(ProcessStartInfo psi = null)
-            {
+        {
             /*
              * ToDo: By experimentation, use this command: git log --pretty=format:"%ae|%an"
              * This will produce an output in this format:
@@ -67,47 +105,16 @@ namespace Tigra.Gravatar.LogFetcher
              */
             const string gitArgumentFormat = "--git-dir=\"{0}\\.git\" log --pretty=format:\"%ae|%an\"";
             if (psi == null)
-                {
+            {
                 psi = new ProcessStartInfo("git.exe");
                 psi.Arguments = string.Format(gitArgumentFormat, GitWorkingCopyPath);
-                }
+            }
             psi.CreateNoWindow = true;
             psi.RedirectStandardError = false;
             psi.RedirectStandardOutput = true;
             psi.UseShellExecute = false;
             var process = Process.Start(psi);
             return process.StandardOutput;
-            }
-
-        public IEnumerable<Committer> GetListOfUniqueCommitters(ProcessStartInfo gitPsi = null)
-            {
-            var logStream = GetLogStream(gitPsi);
-            var uniqueCommitters = new SortedSet<Committer>();
-            var committersAdded = 0;
-            while (!logStream.EndOfStream)
-                {
-                try
-                    {
-                    var logLine = logStream.ReadLine();
-                    Diagnostics.TraceVerbose("Read from log stream => {0}", logLine);
-                    var parts = logLine.Split('|');
-                    if (parts.Length != 2)  // Must hav
-                        continue;
-                    if (string.IsNullOrEmpty(parts[0])) // Must have a non-blank email address
-                        continue;
-                    var committer = new Committer(parts[1], parts[0]);
-                    var added = uniqueCommitters.Add(committer);
-                    Diagnostics.TraceVerbose("Committer={0}, Added={1}", committer,added);
-                    if (added)
-                        ++committersAdded;
-                    }
-                catch (Exception ex)
-                    {
-                    Diagnostics.TraceError(ex);
-                    }
-                }
-            Diagnostics.TraceVerbose("Added a total of {0} committers", committersAdded);
-            return uniqueCommitters;
-            }
         }
     }
+}
--- Tigra.Gravatar.LogFetcher/Options.cs
+++ Tigra.Gravatar.LogFetcher/Options.cs
@@ -1,7 +1,7 @@
 ﻿// This file is part of the Tigra.Gravatar.LogFetcher project
-// 
+//
 // Copyright © 2013 TiGra Networks, all rights reserved.
-// 
+//
 // File: Options.cs  Created: 2013-10-17@07:18
 // Last modified: 2013-10-17@07:52 by Tim
 
@@ -11,27 +11,21 @@ namespace Tigra.Gravatar.LogFetcher
     {
     internal class Options
         {
-        [Option('r', "repository", Required = false, DefaultValue = ".",
+        [Option('r', "repository", Required = false, Default = ".",
             HelpText = "Path to Git repository (working copy)")]
         public string Repository { get; set; }
 
-        [Option('o', "output", Required = false, DefaultValue = @".\Gravatars",
+        [Option('o', "output", Required = false, Default = @".\Gravatars",
             HelpText = "Output directory for Gravatar images")]
         public string OutputDirectory { get; set; }
 
-        [Option('f', "format", Required = false, DefaultValue = "png", HelpText = "Gravatar image format.")]
+        [Option('f', "format", Required = false, Default = "png", HelpText = "Gravatar image format (png, gif, jpg, etc.)")]
         public string ImageFormat { get; set; }
 
-        [Option('s', "size", Required = false, DefaultValue = 90, HelpText = "Size of Gravatar images (in pixels).")]
+        [Option('s', "size", Required = false, Default = 90, HelpText = "Size of Gravatar images (in pixels).")]
         public int GravatarSize { get; set; }
 
-        [Option('v', "verbose", Required = false, DefaultValue = false, HelpText = "Show diagnostic information")]
+        [Option('v', "verbose", Required = false, Default = false, HelpText = "Show diagnostic information")]
         public bool Verbose { get; set; }
-
-        [HelpOption]
-        public string GetUsage()
-            {
-            return "Help coming soon...";
-            }
         }
     }
